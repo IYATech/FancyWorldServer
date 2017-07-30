41b6d1cfd9bc7035f6d27839b829a61bf617ab17
yucongcode <919944@qq.com>
2017-07-30 12:39:29 +0800

    Schema

---
+++ demo/models/activityTheme.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const activityThemeSchema = new Schema({
+    createrId:{type: String, default: ''},
+    activityId:{type: String, default: ''},
+    title:{type: String, default: ''},
+    description:{type: String, default: ''},
+    images:[String],
+    audio:{type: String, default: ''},
+    thumbnail:{type: String, default: ''},
+    video:{type: String, default: ''},
+    sponsor:[String],
+    undertaker:[String],
+    customEnrollInfo:[String],
+    invisibleUserId:[String],
+    visibleUserId:[String],
+    createTime: {type: Date, default: Date.now},
+});
+
+module.export = mongoose.module('activityTheme', activityThemeSchema);
