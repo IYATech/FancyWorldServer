cb054ea2f9bebf24a9ebc9fb533d01846ed8b476
yucongcode <919944@qq.com>
2017-08-01 13:31:05 +0800

    Schema

---
+++ models/kid.js
@@ -0,0 +1,25 @@
+/**
+ * Created by ycong on 2017/8/1.
+ */
+
+/*
+  kid model
+ */
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const kidSchema = new Schema({
+  userId: [{type: Schema.Types.ObjectId, required: true, ref: 'user'}],
+  kidName: {type: String, required: true},
+  kidGender: {type: String, default: ''},
+  kidHobby: [String],
+  kidSchool: {type: String, default: ''},
+  kidClass: {type: String, default: ''},
+  kidTeacherName: [String],
+  kidCounselor: [String],
+  introduction: {type: String, default: ''}
+});
+
+module.exports = mongoose.model('kid', kidSchema);
